9d47f8f01d2629b43c2a24239e30fe167990c0e9
Esat <esat.trem@gmail.com>
2023-02-24 08:23:54 +0300

    created user model

---
+++ server/models/users.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = Schema({
+    id:{
+        type: String,
+        unique: true,
+        required: true,
+        default: Math.floor(Math.random(5) * 1000)
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        timestamps: true
+    },
+    modifiedAt: {
+        type: Date,
+        default: Date.now,
+        timestamps: true
+    },
+    tasks: {
+        type: mongoose.Types.ObjectId,
+    },
+    projects: {
+        type: mongoose.Types.ObjectId,
+    }
+
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
